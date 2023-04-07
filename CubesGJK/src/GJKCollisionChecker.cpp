270ea75abab581a0d360b52e6576354239991d79
Michał Malarski <73782576+mmalarski@users.noreply.github.com>
2023-04-07 19:41:56 +0200

    yeah

--- CubesGJK/src/GJKCollisionChecker.cpp
+++ CubesGJK/src/GJKCollisionChecker.cpp
@@ -1,1 +1,7 @@
 #include "GJKCollisionChecker.h"
+
+glm::vec3 GJKCollisionChecker::findFurthestPointOnDirection(Cube& cube, glm::vec3 direction)
+{
+	std::vector<glm::vec3> vertices = cube.getVertices();
+	return glm::vec3();
+}
--- CubesGJK/src/GJKCollisionChecker.h
+++ CubesGJK/src/GJKCollisionChecker.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "glm/glm.hpp"
+#include "Cube.h"
+
 class GJKCollisionChecker
 {
+public:
+	glm::vec3 findFurthestPointOnDirection(Cube& cube, glm::vec3 direction);
 };
 
